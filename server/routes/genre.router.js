c1cdb00a2c80fd692c424e644262dace8ce9650c
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2020-12-20 17:41:23 -0600

    Genres GET

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require("../modules/pool");
 
-router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+router.get("/", (req, res) => {
+  let sqlText = `SELECT * FROM genres;`;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log("Error fetching genre", error);
+      res.sendStatus(500);
+    });
 });
-
 module.exports = router;
