2d24e7e4b571d12132e798d4fda9ca81b2d27e86
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2020-12-20 17:59:22 -0600

    when trying to click on imagie description not showing

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,25 +1,57 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import MovieItem from "../MovieItem/MovieItem.js";
+
+import { GridList, ListSubheader } from "@material-ui/core";
+import { withStyles } from "@material-ui/core";
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: "150",
+    height: "150",
+  },
+});
+
 class MovieList extends Component {
   componentDidMount() {
-    this.props.dispatch({ type: "FETCH_MOVIE" });
+    console.log("will fetch");
+    this.props.dispatch({ type: "FETCH_MOVIES" });
+    console.log("post fetch");
   }
 
+  getDetails = (id) => {
+    this.props.dispatch({ type: "GET_DETAILS", payload: id });
+    this.props.history.push("/details/");
+  };
+
+  addFavorite = (film) => {
+    this.props.dispatch({ type: "SET_FAVORITE", payload: film });
+  };
+
   render() {
+    const classes = this.props;
+
     return (
-      <div>
-        <ul>
-          {this.props.reduxState.movies.map((film) => (
-            <li key={film.id}>
-              {film.title} <img src={film.poster}></img>
-            </li>
+      <div className={classes.root}>
+        <ListSubheader component="div">MovieList</ListSubheader>
+        <GridList cellHeight={180} className={classes.gridList}>
+          {this.props.reduxState.movies.map((film, index) => (
+            <MovieItem movie={film} getDetails={this.getDetails} />
           ))}
-        </ul>
+        </GridList>
       </div>
     );
   }
 }
+
 const mapReduxStateToProps = (reduxState) => ({
   reduxState,
 });
-export default connect(mapReduxStateToProps)(MovieList);
+export default withStyles(styles)(connect(mapReduxStateToProps)(MovieList));
